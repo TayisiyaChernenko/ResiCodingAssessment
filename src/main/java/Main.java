0b84cbbda1ac5eb2a7923781ceaeeb9d3a2f5a37
Tayisiya Chernenko <tayisiyachernenko@Tayisiyas-Air.attlocal.net>
2025-04-13 12:44:17 -0500

    Dates are read from file and normalized

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,38 @@
 package main.java;
+import java.io.*;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
 
 public class Main {
-    public static void main(String[] args){
-        String normDate = DateNormalization.normalize("June 2, 2018");
-        System.out.println(normDate);
+
+    public static void fetch_image(String validDate){
+        System.out.println(validDate);
     }
+    public static void run(){
+        try {
+            File file = new File(Main.class.getClassLoader().getResource("dates.txt").getFile());
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()){   
+                String rawDate = sc.nextLine();
+                String normDate = DateNormalization.normalize(rawDate);
+                if(normDate.equals("Invalid date")){
+                    System.out.println(rawDate + " is invalid");
+                }
+                else{
+                    fetch_image(normDate);
+                }
+            }   
+        sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        run();
+}
 }
